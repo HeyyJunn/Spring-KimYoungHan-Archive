693d7e657f99489384235d066d8bd4916ff22d44
xxanecdote@naver.com <xxanecdote@naver.com>
2025-03-29 21:26:58 +0900

    Update

--- hello-spring/src/main/java/hello/hello_spring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hello_spring/controller/HelloController.java
@@ -7,9 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
 
-    @GetMapping("hello") // web application 에서 /hello 라고 들어오면 이 메서드를 호출
+    @GetMapping("/hello") // web application 에서 /hello 라고 들어오면 이 메서드를 호출
     public String hello(Model model) {
         model.addAttribute("data", "Hello World");
         return "hello";
     }
 }
+
